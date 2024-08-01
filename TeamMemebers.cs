2e235b45426fededf0ba99937269d57f5dcf0b8d
Chelsea Brooks <cbrooks6212@gmail.com>
2024-08-01 10:21:33 -0400

    modify

---
+++ TeamMemebers.cs
@@ -0,0 +1,13 @@
+
+public class TeamMember
+{
+    public string? Name { get; set; }
+    public int SkillLevel { get; set; }
+    public double CourageFactor { get; set; }
+    public TeamMember(string name, int skillLevel, double courageFactor)
+    {
+        Name = name;
+        SkillLevel = skillLevel;
+        CourageFactor = courageFactor;
+    }
+}
